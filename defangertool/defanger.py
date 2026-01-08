f8b42e8db02f7b2b2a667c36a170e02e96911ccb
Melisa Nyamukondiwa <melissanyk@gmail.com>
2026-01-08 23:59:59 +0200

    Update defanger.py

--- defangertool/defanger.py
+++ defangertool/defanger.py
@@ -4,23 +4,28 @@ start = input("Do you want to defang an ip or url? (I/U): ")
 
 #When a user decides to defang an ip address
 if start == "I":
-    ip = input("Enter the ip address: ")
-    if ip.isalpha():
-        print("Invalid input. Enter a valid ip address")
+    ip = input("Enter the ip address: ") #prompt to enter the ip address
+    if ip.isalpha():     #Validates inpurt, preventing a user from entering letters instead of ip address
+        print("Invalid input. Enter a valid ip address") 
+    #If user inputs a valid ip address they defanging process starts
+    #The replace function is used to defang
     else:
          defanged_ip = ip.replace(".", "[.]")
          defanged_ip = defanged_ip.replace("/", "[/]")
          print(f"Normal: {ip}") 
-         print(f"Defanged: {defanged_ip}")                           
+         print(f"Defanged: {defanged_ip}")     
+#When a user wants to defang a url. Will not show if user selected I
 elif start == "U":
-    url = input("Enter the url: ")
+    url = input("Enter the url: ") 
+    #iterates through common symbols to get the end result
     defanged_url = url.replace(".", "[.]")
     defanged_url = defanged_url.replace("://", "[://]")
     defanged_url = defanged_url.replace("http", "hxxp")
     defanged_url = defanged_url.replace("ftp", "fxp")
     print(f"Normal: {url}")
     print(f"Defanged: {defanged_url}") 
-
+#If user types anything other than I or U when asked if they want to defang an ip or url
 else:
 
     print("Invalid choice, pick I/U: ")
+
